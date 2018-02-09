d9ef4ec85f26a7505e7f56f8cb13948efbdb3006
Max Tsao <mtsao@YVR-NBM1056.local>
2018-02-08 17:11:17 -0800

    test

---
+++ findinheaders.py
@@ -0,0 +1,43 @@
+import os
+import fnmatch
+import re
+
+print "What is the header name you are trying to find?";
+header_name = raw_input();
+delimiter = "";
+
+def get_delimiter():
+    print "What is the file delimited by? Options: Tab Pipe Comma";
+    global delimiter;
+    delimiter = raw_input().lower();
+    if delimiter == "tab":
+        delimiter = "\t";
+    elif delimiter == "pipe":
+        delimiter = "|";
+    elif delimiter == "comma":
+        delimiter = ",";
+    elif delimiter == "space":
+        delimiter = " ";
+    else:
+        print "Please choose from the following choices: tab pipe comma";
+        get_delimiter();
+
+def find_name_in_header(header_line, file_path):
+    file_header_names = header_line.split(delimiter);
+    #print file_header_names;
+    for name in file_header_names:
+        if header_name.lower() in name.lower():
+            print "Found in: " + file_path;
+
+
+def read_all_files_in_folders():
+    for root, dirs, files in os.walk('Regular Loads/'):
+         for file in files:
+            with open(os.path.join(root, file), "r") as auto:
+                header_line = auto.readline();
+                file_path = os.path.join(root, file);
+                find_name_in_header(header_line, file_path);
+                #print header_line;
+
+get_delimiter();
+read_all_files_in_folders();
